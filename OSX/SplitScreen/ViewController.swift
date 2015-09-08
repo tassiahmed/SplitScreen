e7ed492655658e8490d078a5dcfbe95f4561f9a7
Tausif Ahmed <atausif00@gmail.com>
2015-09-08 17:12:02 -0400

    Initial Project Files for OSX Application

---
+++ OSX/SplitScreen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SplitScreen
+//
+//  Created by Tausif Ahmed on 9/8/15.
+//  Copyright (c) 2015 SplitScreen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
---
+++ OSX/SplitScreenTests/SplitScreenTests.swift
@@ -0,0 +1,36 @@
+//
+//  SplitScreenTests.swift
+//  SplitScreenTests
+//
+//  Created by Tausif Ahmed on 9/8/15.
+//  Copyright (c) 2015 SplitScreen. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class SplitScreenTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
